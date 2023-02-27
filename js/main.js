dacff04a32ee6bcc5cae3ad5f649e927f8b19301
LucaBrivio183 <l.brivio4@gmail.com>
2023-02-27 17:29:52 +0100

    feat: next  button functioning

--- js/main.js
+++ js/main.js
@@ -23,8 +23,38 @@ for (let i = 0; i < images.length; i++) {
     </div>`;
 }
 
+//add item in the slider section
+
 slider.innerHTML +=  item;
 
 //temporary show the first img
 
 document.querySelector('.item').classList.add('show');
+
+//define prev and next button variable
+
+const nextButton = document.querySelector('.next');
+const prevButton = document.querySelector('.prev');
+
+//define collection of imgs
+
+const itemsCollection = document.querySelectorAll('.item');
+
+//define an active index variable
+
+let active = 0;
+//define click event of next button
+
+nextButton.addEventListener('click',function(){
+    
+    if (active < itemsCollection.length - 1){
+            itemsCollection[active].classList.remove('show');
+            active++;
+            itemsCollection[active].classList.add('show');
+        }
+
+});
+
+//define click event of prev button
+
+
