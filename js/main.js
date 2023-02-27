2831d88e2cca344189f10cfe5fe6b06bd210593b
LucaBrivio183 <l.brivio4@gmail.com>
2023-02-27 16:54:29 +0100

    feat: milestone #2

--- js/main.js
+++ js/main.js
@@ -2,3 +2,29 @@
 
 'use strict'
 
+//define array of img used
+
+const images = ['01.webp','02.webp','03.webp','04.webp','05.webp'];
+
+//define item variable
+
+let item = '';
+
+//define slider variable
+
+const slider =  document.querySelector('.slider');
+
+//cycle trough all img for creating html elements with code
+
+for (let i = 0; i < images.length; i++) {
+
+   item += `<div class="item">
+   <img src="img/${images[i]}" alt="">
+    </div>`;
+}
+
+slider.innerHTML +=  item;
+
+//temporary show the first img
+
+document.querySelector('.item').classList.add('show');
